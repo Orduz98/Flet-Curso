09a45c53e3059fbce7c469e5089fdd8a2167c1b8
Esteban Orduz Balvin <87547350+Orduz98@users.noreply.github.com>
2024-04-08 14:51:55 -0500

    demo inicial contador en flet

--- demo-inicial-contador/counter.py
+++ demo-inicial-contador/counter.py
@@ -1,21 +1,20 @@
 import flet
 from flet import IconButton, Page, Row, TextField, icons
 
-
-def main(page: Page):
-    page.title = 'Flet Counter'
+def main (page: Page):
+    page.title = "Contador Flet"
     page.vertical_alignment = 'center'
 
     txt_number = TextField(value='0', text_align='right', width=100)
 
     def minus_click(event):
-        txt_number.value = int(txt_number.value) - 1
+        txt_number.value = int(txt_number.value)-1
         page.update()
-    
+
     def plus_click(event):
-        txt_number.value = int(txt_number.value) + 1
+        txt_number.value = int(txt_number.value)+1
         page.update()
-    
+
     page.add(
         Row(
             [
@@ -27,9 +26,8 @@ def main(page: Page):
         )
     )
 
-
-# Modo Desktop:
+# Modo Desktop
 # flet.app(target=main)
-
-# Modo Web:
+    
+# Modo Pagina Web
 flet.app(target=main, view=flet.WEB_BROWSER)
